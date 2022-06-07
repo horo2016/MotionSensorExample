3b06f34a44290f227762ac811eb9dfd0ccc8bc1a
horo2016 <selectld@163.com>
2022-06-07 13:31:47 +0800

    Update sensor.h

--- MotionSensor/sensor.h
+++ MotionSensor/sensor.h
@@ -1,11 +1,24 @@
 #ifndef SENSOR_H
 #define SENSOR_H
+#include "helper_3dmath.h"
+#define YAW 0
+#define PITCH 1
+#define ROLL 2
+#define DIM 3
+extern float ypr[3]; //yaw, pitch, roll
+extern float accel[3];
+extern float gyro[3];
+extern float temp;
+extern float compass[3];
 
-int ms_open();
-int ms_update();
-int ms_close();
+extern int ms_open();
+extern int ms_update();
+extern int ms_close();
 
-uint8_t GetGravity(VectorFloat *v, Quaternion *q);
-uint8_t GetYawPitchRoll(float *data, Quaternion *q, VectorFloat *gravity); 
-uint8_t GetGyro(int32_t *data, const uint8_t* packet);
+
+extern unsigned char GetGravity(VectorFloat *v, Quaternion *q);
+extern unsigned char GetYawPitchRoll(float *data, Quaternion *q, VectorFloat *gravity); 
+extern unsigned char GetGyro(int32_t *data, const unsigned char* packet);
+
+
 #endif
